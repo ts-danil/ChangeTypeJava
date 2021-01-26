2a085975363c650edb9a24b8e7f87a7ae92d50be
Danil <danil1198@yandex.ru>
2021-01-27 00:51:30 +0300

    Получение данных от пользователя

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Введите символ: ");
+    Scanner symbol = new Scanner(System.in);
+    System.out.println("Ваш символ: " + symbol.nextLine());
+    }
+}
