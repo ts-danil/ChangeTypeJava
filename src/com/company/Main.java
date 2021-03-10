6380a5501032cb9a13eb4ad400193b9664cc5ddb
Danil <danil1198@yandex.ru>
2021-03-11 00:15:59 +0300

    Упрощение кода, удаление лишней переменной

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char symbol = ' ';
-        int ascii = 1;
         System.out.println("Введите символ: ");
-        if (sc.hasNextLine()) {
-            symbol = sc.nextLine().charAt(0);
-            ascii = symbol;
-        }
-        System.out.println("Ваш символ: " + symbol + "\nКод символа ASCII: " + ascii);
+        char symbol = sc.nextLine().charAt(0);
+        System.out.println("Ваш символ: " + symbol + "\nКод символа ASCII: " + (int) symbol);
     }
 }
